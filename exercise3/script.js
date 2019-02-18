81bc9c7d81f89eafa1321d0d87a2876ffcf9ace6
samadova.minira <minira.samadova@syncthought.com>
2019-02-18 14:48:00 +0900

    at last I solved exercise3

---
+++ exercise3/script.js
@@ -0,0 +1,47 @@
+function zFunction() {
+  var inputNumber = Number(prompt("Can you pick a number?"));
+  var result = 1;
+  while(inputNumber > 0) {
+    result += inputNumber;
+    inputNumber--;
+  }
+  return(result);
+}
+
+alert(zFunction())
+
+
+
+
+/** Write a program that asks the user for a number n and prints the sum of the numbers 1 to n
+
+var result = 1;
+if(inputNumber === 0;) {
+  while(inputNumber > 0) {
+  result += 1;
+  inputNumber--;
+  }
+} else {
+  return("OMG, can't you even enter a NUMBER?");
+}
+
+
+
+
+we have a number 7
+var number = 7;
+I need the sum of all numbers from 1 to 7
+
+1 + n-1 + n-2 + n-3 + .. + n-n
+
+  while(number > 0) {
+    var result2 = number - 1;
+    result--;
+  }
+  return(result);
+}
+
+1 + (result - 1) + (result - 2)
+
+
+alert(loop(7)); **/
