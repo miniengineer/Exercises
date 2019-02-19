e02007ec8bfcae09affa5e6d47b34ca09079f025
samadova.minira <minira.samadova@syncthought.com>
2019-02-19 11:24:34 +0900

    code style reviewed

--- exercise2/script.js
+++ exercise2/script.js
@@ -1,17 +1,11 @@
-function askTheName(name) {
-  var result = prompt(name);
-  if (result == "Alice" || result == "Bob") {
-    result += "!";
-    alert("I have been waiting for you, " + result);
+function askTheName(question) {
+  var name = prompt(name);
+  if (name == "Alice" || name == "Bob") {
+    name += "!";
+    alert("I have been waiting for you, " + name);
   } else {
     alert("You are not the chosen one!!!");
   }
 }
 
 askTheName ("Can I have your name please?");
-
-
-
-
-
-//Modify the previous program such that only the users Alice and Bob are greeted with their names.
