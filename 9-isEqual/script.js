436d1893f2c1910e3f8669ed5cca11e3933daddb
samadova.minira <minira.samadova@syncthought.com>
2019-02-22 19:01:18 +0900

    deleted unnecessary parts

--- 9-isEqual/script.js
+++ 9-isEqual/script.js
@@ -1,3 +1,5 @@
+debugger;
+
 function isEqual(str1,str2) {
   if(str1.length === str2.length) {
     return comparison(str1,str2);
@@ -7,13 +9,9 @@ function isEqual(str1,str2) {
 }
 
 function comparison(str1,str2) {
-  var letter1 = "";
-  var letter2 = "";
   var letterPosition = 0;
   while(letterPosition < str1.length) {
-    letter1 = str1[letterPosition];
-    letter2 = str2[letterPosition];
-    if(letter1 === letter2) {
+    if(str1[letterPosition] === str2[letterPosition]) {
       letterPosition++;
     } else {
       return false;
@@ -21,4 +19,4 @@ function comparison(str1,str2) {
   } return true;
 }
 
-isEqual("минира is smart","минира is smart");
+console.log(isEqual("минара is smart","минира is smart"));
