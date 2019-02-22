0352ed7120aa0b41cadad1b141d096a6e018de00
samadova.minira <minira.samadova@syncthought.com>
2019-02-22 11:38:43 +0900

    8-return-the-number done

--- 8-return-the-number/script.js
+++ 8-return-the-number/script.js
@@ -1,8 +1,15 @@
 //You are given text in English. Write a JavaScript function that accepts text as an argument, and returns the number of word in this text.
 
-function f1(text) {
-  var count = text.length;
-  document.write(count);
+function wordCount(text) {
+  var currentPosition = 0;
+  var count = 1;
+  while(currentPosition < text.length) {
+    if(text[currentPosition] === " ") {
+      count++;
+    }
+    currentPosition++;
+  }
+  return count;
 }
 
-f1("Hello!");
+document.write(wordCount("My husband is the best"));
