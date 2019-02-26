3a935db62432710111a28725b5d412c452c6d939
samadova.minira <minira.samadova@syncthought.com>
2019-02-26 10:12:48 +0900

    bubble sort done

---
+++ 14-bubble-sort-algorithm/script.js
@@ -0,0 +1,20 @@
+//Write bubble sort algorithm
+debugger;
+
+function bubbleSort(array) {
+  var counter = 1;
+  var keepForNow = 0;
+  while(counter > 0) {
+    for(var i = 0; i < array.length - 1; i++) {
+      if(array[i] > array[i + 1]) {
+        keepForNow = array[1];
+        array[i] = array[i+1];
+        array[i+1] = keepForNow;
+        counter++;
+      }
+    } counter = 0;
+  }
+  return array;
+}
+
+console.log(bubbleSort([1,3,2,5,6,8]));
