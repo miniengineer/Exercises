cac779d67d72d18efff8706773b6f11272374acb
samadova.minira <minira.samadova@syncthought.com>
2019-03-04 17:19:52 +0900

    25-return-factorial-of-n

---
+++ 25-return-factorial-of-n/script.js
@@ -0,0 +1,25 @@
+// Challenge
+// Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it.
+// For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. For the test cases,
+// the range will be between 1 and 18 and the input will always be an integer.
+//
+// Sample Test Cases
+// Input:4
+// Output:24
+//
+//
+// Input:8
+// Output:40320
+
+function firstFactorial(num) {
+  var array = [];
+  for(var i = 1; i < num; i++) {
+    array.push(num - i);
+  }
+  for(var i = 0; i < array.length; i++) {
+    num *= array[i];
+  }
+  return num;
+}
+
+firstFactorial(8);
