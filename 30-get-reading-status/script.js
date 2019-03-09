da87e2d4a298f2445ecb73096a9f9378b7293a96
samadova.minira <minira.samadova@syncthought.com>
2019-03-09 16:00:26 +0900

    30-get-reading-status

---
+++ 30-get-reading-status/script.js
@@ -0,0 +1,26 @@
+function showStatus(array) {
+  var result = "";
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].readingStatus) {
+      result += "The book by " + array[i].author + " named " + array[i].title + " was read." + "\n";
+    }
+    else {
+      result += "You are still reading " + "the " + array[i].title  + " by " + array[i].author + "\n";
+    }
+  }
+  return result;
+}
+
+var library = [
+  {
+    author: "Bill Gates",
+    title: "The Road Ahead",
+    readingStatus: true
+  },
+  {
+    author: "Steve Jobs",
+    title: "Walter Isaacson",
+    readingStatus: false
+  }];
+
+showStatus(library);
