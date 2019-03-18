624eea7fd93eb4e6bfded9d76c80da58d22e933c
samadova.minira <minira.samadova@syncthought.com>
2019-03-18 09:26:55 +0900

    new exercise

---
+++ 36-change-first-digits-to-hash/script.js
@@ -0,0 +1,35 @@
+// Credit Card Mask
+//
+// Usually when you buy something, you're asked whether your credit card number,
+// phone number or answer to your most secret question is still correct.
+// However, since someone could look over your shoulder, you don't want that shown on your screen.
+// Instead, we mask it.
+//
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+//
+// Examples
+// maskify("4556364607935616") == "############5616"
+// maskify(     "64607935616") ==      "#######5616"
+// maskify(               "1") ==                "1"
+// maskify(                "") ==                 ""
+//
+// // "What was the name of your first pet?"
+// maskify("Skippy")                                   == "##ippy"
+// maskify("Nananananananananananananananana Batman!") == "####################################man!"
+//
+//
+
+
+function maskify(string) {
+  if (string === "" || string.length < 2) {
+    return string;
+  }
+  var array = string.split("");
+  for (var i = 0; i < array.length - 4; i++) {
+    array[i] = "#";
+  }
+  string = array.join("");
+  return string;
+}
+
+maskify("Nananananananananananananananana Batman!");
