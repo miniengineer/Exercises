aa4d991b3633c8871b6520d21da76eda0805c0f2
samadova.minira <minira.samadova@syncthought.com>
2019-03-23 14:40:32 +0900

    exercise done

--- 41-voters-age-range/script.js
+++ 41-voters-age-range/script.js
@@ -5,40 +5,46 @@
 // See the example output at the bottom.
 
 var voters = [
-    {name:'Bob' , age: 30, voted: true},
-    {name:'Jake' , age: 32, voted: true},
-    {name:'Kate' , age: 25, voted: false},
-    {name:'Sam' , age: 20, voted: false},
-    {name:'Phil' , age: 21, voted: true},
-    {name:'Ed' , age:55, voted:true},
-    {name:'Tami' , age: 54, voted:true},
-    {name: 'Mary', age: 31, voted: false},
-    {name: 'Becky', age: 43, voted: false},
-    {name: 'Joey', age: 41, voted: true},
-    {name: 'Jeff', age: 30, voted: true},
-    {name: 'Zack', age: 19, voted: false}
+  {name:"Bob" , age: 30, voted: true},
+  {name:"Jake" , age: 32, voted: true},
+  {name:"Kate" , age: 25, voted: false},
+  {name:"Sam" , age: 20, voted: false},
+  {name:"Phil" , age: 21, voted: true},
+  {name:"Ed" , age:55, voted:true},
+  {name:"Tami" , age: 54, voted:true},
+  {name: "Mary", age: 31, voted: false},
+  {name: "Becky", age: 43, voted: false},
+  {name: "Joey", age: 41, voted: true},
+  {name: "Jeff", age: 30, voted: true},
+  {name: "Zack", age: 19, voted: false}
 ];
 
 function voterResults(arr) {
-   arr.reduce((a,b) => {
-     if(b.age < 26) {
-       if(b.voted) {
-         return a.youngVotes++;
-       };
-       return a.youth++;
-     } else if(b.age > 25 && b.age < 36) {
-       if(b.voted) {
-         return a.midVotes++;
-       };
-       return a.mids++;
-     } else if(b.age > 35 && b.age < 56) {
-       if(b.voted) {
-         return a.oldVotes++;
-       };
-       return a.olds++;
-     };
-   }, {youngVoters: 0, youth: 0, midVoters: 0, mids: 0, oldVoters: 0, olds: 0});
-};
+  return arr.reduce((a,b) => {
+    if(b.age < 26) {
+      if(b.voted) {
+        a.youngVotes++;
+        return a;
+      }
+      a.youth++;
+      return a;
+    } else if(b.age > 25 && b.age < 36) {
+      if(b.voted) {
+        a.midVotes++;
+        return a;
+      }
+      a.mids++;
+      return a;
+    } else if(b.age > 35 && b.age < 56) {
+      if(b.voted) {
+        a.oldVotes++;
+        return a;
+      }
+      a.olds++;
+      return a;
+    }
+  }, {youngVotes: 0, youth: 0, midVotes: 0, mids: 0, oldVotes: 0, olds: 0});
+}
 
 console.log(voterResults(voters));
 // Returned value shown below:
