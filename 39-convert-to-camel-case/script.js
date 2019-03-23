5c081a6025ce862aeb47445a905cebc1d542449f
samadova.minira <minira.samadova@syncthought.com>
2019-03-23 23:26:12 +0900

    fixed with lint

--- 39-convert-to-camel-case/script.js
+++ 39-convert-to-camel-case/script.js
@@ -6,18 +6,6 @@
 //
 // toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior"
 
-
-function convertToCamelCase(string) {
-  var regex = /(?<=[-])[a-z]/;
-  while (string.search(/[-]/) !== -1) {
-    var lowerCase = string[string.search(regex)];
-    var upperCase = string[string.search(regex)].toUpperCase();
-    string = string.replace(lowerCase,upperCase);
-    string = string.replace(/[-]/,"");
-  }
-  return string;
-}
-
 function toCamelCase(string) {
   return string.replace(/-([a-z])/g, function(match) {
     return match[1].toUpperCase();
@@ -25,29 +13,3 @@ function toCamelCase(string) {
 }
 
 console.log(toCamelCase("the-best-warrior"));
-
-
-
-// var myName = ["Minira","Samadova"];
-//
-// function join(array,separator = " ") {
-//   var string = "";
-//   if (array == null || array.length == 0) {
-//     return string;
-//   }
-//   for(var i = 0; i < array.length-1;i++) {
-//     string += array[i];
-//     string += separator;
-//   }
-//   string += array[array.length-1];
-//   return string;
-// };
-//
-// If no separator is provided, the default should be a string space ' '.
-// And it will return an empty string if no array is provided as the first input.
-//
-// join("&")
-//
-// function show(variable,number) {
-//   var string = "";
-// }
