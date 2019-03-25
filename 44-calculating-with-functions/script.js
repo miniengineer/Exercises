7ba935c3797cd98d223cb82136cf0872ae1aeeb1
samadova.minira <minira.samadova@syncthought.com>
2019-03-26 07:49:53 +0900

    maybe now lint will be happy

--- 44-calculating-with-functions/script.js
+++ 44-calculating-with-functions/script.js
@@ -1,25 +1,25 @@
-Calculating with Functions
-
-This time we want to write calculations using functions and get the results. Let's have a look at some examples:
-
-JavaScript:
-
-seven(times(five())); // must return 35
-four(plus(nine())); // must return 13
-eight(minus(three())); // must return 5
-six(dividedBy(two())); // must return 3
-
-Requirements:
-
-There must be a function for each number from 0 ("zero") to 9 ("nine")
-There must be a function for each of the following mathematical operations:
-plus, minus, times, dividedBy
-Each calculation consist of exactly one operation and two numbers
-The most outer function represents the left operand, the most inner function represents the right operand
-Divison should be integer division, e.g eight(dividedBy(three()))/eight(divided_by(three))
-should return 2, not 2.666666...
-
-seven(times(five())); // must return 35
+// Calculating with Functions
+//
+// This time we want to write calculations using functions and get the results. Let's have a look at some examples:
+//
+// JavaScript:
+//
+// seven(times(five())); // must return 35
+// four(plus(nine())); // must return 13
+// eight(minus(three())); // must return 5
+// six(dividedBy(two())); // must return 3
+//
+// Requirements:
+//
+// There must be a function for each number from 0 ("zero") to 9 ("nine")
+// There must be a function for each of the following mathematical operations:
+// plus, minus, times, dividedBy
+// Each calculation consist of exactly one operation and two numbers
+// The most outer function represents the left operand, the most inner function represents the right operand
+// Divison should be integer division, e.g eight(dividedBy(three()))/eight(divided_by(three))
+// should return 2, not 2.666666...
+//
+// seven(times(five())); // must return 35
 
 function times(number) {
   return function(a) {
@@ -28,14 +28,14 @@ function times(number) {
 }
 
 function makeOperand(operand) {
-    return function(calculation) {
-      if(arguments.length < 1) {
-        return operand;
-      } else {
-        return calculation(operand);
-      };
-    };
+  return function(calculation) {
+    if(arguments.length < 1) {
+      return operand;
+    } else {
+      return calculation(operand);
+    }
+  };
 }
 
-const five = makeOperand(5);
-const four = makeOperand(4);
+times(4);
+makeOperand(4);
