af772812732cefe4ac65afd8d8fc6c290abb7fc5
samadova.minira <minira.samadova@syncthought.com>
2019-03-27 22:48:25 +0900

    fixed bill first choice bug

--- 40-vasya-clerk/script.js
+++ 40-vasya-clerk/script.js
@@ -30,10 +30,10 @@ function vasyaClerk(arr) {
       }
     } else if(arr[i] === 100) {
       if(object.twentyFive > 0 && object.fifty > 0) {
-        object.twentyFive -= 3;
-      } else if(object.twentyFive > 3 || object.twentyFive > 0 && object.fifty > 0) {
         object.twentyFive--;
         object.fifty--;
+      } else if(object.twentyFive > 2) {
+        object.twentyFive -= 3;
       } else {
         return false;
       }
