f763d3bf32553b8eda53f905ee058b64e81cfab1
samadova.minira <minira.samadova@syncthought.com>
2019-03-27 11:59:02 +0900

    reviewed

--- 40-vasya-clerk/script.js
+++ 40-vasya-clerk/script.js
@@ -29,9 +29,9 @@ function vasyaClerk(arr) {
         return false;
       }
     } else if(arr[i] === 100) {
-      if(object.twentyFive > 3 || object.twentyFive > 0 && object.fifty > 0) {
+      if(object.twentyFive > 0 && object.fifty > 0) {
         object.twentyFive -= 3;
-      } else if(object.twentyFive > 0 && object.fifty > 0) {
+      } else if(object.twentyFive > 3 || object.twentyFive > 0 && object.fifty > 0) {
         object.twentyFive--;
         object.fifty--;
       } else {
