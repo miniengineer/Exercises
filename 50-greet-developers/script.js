0d188598731f78238af15c27a2ddadae83d66dd0
Minira <Minira@Miniras-MacBook-Air.local>
2019-04-05 13:13:44 +0900

    Initialized npm; Added lodash

--- 50-greet-developers/script.js
+++ 50-greet-developers/script.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 // You will be given an array of objects (associative arrays in PHP) representing data
 // about developers who have signed up to attend the next coding meetup that you are organising.
 //
@@ -34,9 +35,10 @@ var list1 = [
 ];
 
 function addGreeting(array) {
-  let result = JSON.parse(JSON.stringify(array));
+  let result = _.cloneDeep(array);
   result.forEach(e => e.greeting = `Hi ${e.firstName}, what do you like the most about ${e.language}?`);
   return result;
 }
 
-addGreeting(list1);
+console.log(addGreeting(list1));
+console.log(list1);
