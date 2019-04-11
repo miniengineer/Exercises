d45bb523ebd49c696adc72d9331dbc835ae7347f
samadova.minira <minira.samadova@syncthought.com>
2019-04-11 12:23:30 +0900

    refactored the code

--- 51-number-of-people-from-europe/script.js
+++ 51-number-of-people-from-europe/script.js
@@ -26,7 +26,7 @@
 function whoIsFromEurope(array) {
   let result = array.reduce((a,b) => {
     if(b.continent === "Europe" && b.language === "JavaScript") {
-      return a+1;
+      return a + 1;
     } else {
       return a;
     }
