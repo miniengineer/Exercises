a5fdac2729eee8441165bf544aa5fc90f36cab6b
Minira <Minira@Miniras-MacBook-Air.local>
2019-04-17 08:14:24 +0900

    find day and time

---
+++ 58-date-and-time/script.js
@@ -0,0 +1,15 @@
+function timeAndDate(integer) {
+  let hours = Math.floor(integer / 60);
+  var minutes = integer % 60;
+
+  if (integer > 0) {
+    if (hours < 24) {
+      hours = 00 + hours;
+    }
+  } else if (integer < 0) {
+
+  }
+
+}
+
+https://www.codewars.com/kata/56fac4cfda8ca6ec0f001746
