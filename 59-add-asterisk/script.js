9b19fb04a5746856feac4057647f5250b116631a
samadova.minira <minira.samadova@syncthought.com>
2019-04-17 13:39:50 +0900

    needs check

--- 59-add-asterisk/script.js
+++ 59-add-asterisk/script.js
@@ -1,4 +1,14 @@
-debugger;
+// You are given a function that must insert an asterisk between every pair of even digits in an integer n,
+// that should be converted to a string(toString), received as parameter and return it.
+//
+// n can be a string/an array containing integers only.
+//
+// If n is an array, put the elements together then put the asterisk, treating them as a string.
+// e.g. [1, 2, 4, 8, 16] ==> "12*4*816"
+//
+// Here we will consider all the numbers that n % 2 == 0 as even numbers.
+//
+// Have fun!
 
 function asteriskIt(numbers) {
   if (typeof numbers === 'string') {
@@ -7,11 +17,11 @@ function asteriskIt(numbers) {
   numbers.forEach(function (currentElement, currentIndex) {
     if (currentElement % 2 === 0 && numbers[currentIndex + 1] % 2 === 0) {
       numbers.splice(currentIndex + 1,0,'*');
-      console.log(numbers);
     }
   });
+  numbers = numbers.join("");
   return numbers;
 }
 
 
-console.log(asteriskIt([1, 2, 4, 8, 16, 4]));
+(asteriskIt([1, 2, 4, 8, 16, 4]);
