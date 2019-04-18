d74a9b44f6a20eb7a13982f2427d51e76b5b5c52
samadova.minira <minira.samadova@syncthought.com>
2019-04-18 14:56:34 +0900

    align the code with the expected return

--- 60-mexican-wave/script.js
+++ 60-mexican-wave/script.js
@@ -1,24 +1,31 @@
 // In this simple Kata your task is to create a function that turns a string into a Mexican Wave.
 // You will be passed a string and you must return that string in an array
 // where an uppercase letter is a person standing up.
-
+//
+// Example:
+// "hello" => ["Hello", "hEllo", "heLlo", "helLo", "hellO"];
 
 function wave(str) {
   if (str.length < 1) {
     return [];
   }
 
-  let wavedArr = str.split("");
-  let result = wavedArr.reduce((accumulator,currentElement,currentIndex) => {
-    accumulator.push([...wavedArr]);
-    if(currentElement === " ") {
-      return accumulator;
+  str = str.split("");
+  let array = [...str];
+  let result = [];
+  array.forEach((currentElement, currentIndex) => {
+    result.push([...str]);
+    if (currentElement === " ") {
+      return " ";
     } else {
-      accumulator[currentIndex][currentIndex] = currentElement.toUpperCase();
+      result[currentIndex][currentIndex] = currentElement.toUpperCase();
     }
-    return accumulator
-  }, [])
+  });
+  result = result.map(element => {
+    element = element.join("");
+    return element;
+  });
   return result;
 }
 
-wave("hello");
+wave("minira");
