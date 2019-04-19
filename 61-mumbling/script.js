103119bcf0738e0b5e38aa87d802d81f1c8a202a
samadova.minira <minira.samadova@syncthought.com>
2019-04-19 13:17:33 +0900

    practising string methods

---
+++ 61-mumbling/script.js
@@ -0,0 +1,30 @@
+// Mumbling.
+//
+// This time no story, no theory. The examples below show you how to write function accum:
+//
+// Examples:
+//
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+//
+// link: https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
+
+function accum(string) {
+  string = string.split("");
+  let result = string.reduce((accumulator,currentElement,i) => {
+    if (i === 0) {
+      currentElement = currentElement.toUpperCase();
+      accumulator.push(currentElement);
+    } else {
+      let accumLetter = currentElement.padEnd(i, currentElement);
+      accumLetter = accumLetter[0].toUpperCase().concat(accumLetter);
+      accumulator.push(accumLetter);
+    }
+    return accumulator;
+  }, []).join("-");
+  return result;
+}
+
+accum("abcd");
