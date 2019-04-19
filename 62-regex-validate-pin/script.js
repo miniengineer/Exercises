fa344fb1662be243bfa5a7c80d2ef8fe4260efbf
samadova.minira <minira.samadova@syncthought.com>
2019-04-19 14:44:41 +0900

    first time using regEx method

---
+++ 62-regex-validate-pin/script.js
@@ -0,0 +1,20 @@
+// Regex validate PIN code.
+//
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything
+// but exactly 4 digits or exactly 6 digits.
+//
+// If the function is passed a valid PIN string, return true, else return false.
+//
+// eg:
+//
+// validatePIN("1234") === true
+// validatePIN("12345") === false
+// validatePIN("a234") === false
+//
+// link: https://www.codewars.com/kata/55f8a9c06c018a0d6e000132
+
+
+const validatePIN = (string) => /^(\d{4}|\d{6})$/.test(string);
+
+
+validatePIN("1234");
