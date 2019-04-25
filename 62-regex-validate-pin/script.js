cc9c8eac5288983fe15096da9dc96234b758fa55
samadova.minira <minira.samadova@syncthought.com>
2019-04-25 11:29:57 +0900

    deleted unnecessary capture groups

--- 62-regex-validate-pin/script.js
+++ 62-regex-validate-pin/script.js
@@ -14,7 +14,7 @@
 // link: https://www.codewars.com/kata/55f8a9c06c018a0d6e000132
 
 
-const validatePIN = (string) => /^(\d{4}|\d{6})$/.test(string);
+const validatePIN = (string) => /^\d{4}|\d{6}$/.test(string);
 
 
-validatePIN("1234");
+console.log(validatePIN("1234"));
