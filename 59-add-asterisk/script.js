28fb23a6eb247a3790352d495b72874f6ac0a89f
Minira Samadova <samadova.minira@gmail.com>
2019-07-07 18:10:34 +0900

    lint fix

--- 59-add-asterisk/script.js
+++ 59-add-asterisk/script.js
@@ -1,12 +1,10 @@
-debugger;
-
 function asteriskIt(numbers) {
-  if (typeof numbers === 'string') {
-    numbers = numbers.split('');
+  if (typeof numbers === "string") {
+    numbers = numbers.split("");
   }
-  numbers.forEach(function (currentElement, currentIndex) {
+  numbers.forEach(function(currentElement, currentIndex) {
     if (currentElement % 2 === 0 && numbers[currentIndex + 1] % 2 === 0) {
-      numbers.splice(currentIndex + 1,0,'*');
+      numbers.splice(currentIndex + 1,0,"*");
       console.log(numbers);
     }
   });
@@ -14,4 +12,4 @@ function asteriskIt(numbers) {
 }
 
 
-console.log(asteriskIt([1, 2, 4, 8, 16, 4]));
+asteriskIt([1, 2, 4, 8, 16, 4]);
