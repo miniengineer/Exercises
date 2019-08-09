ee6fff3a6790f4a0efb32c59a1d39e907524046f
samadova.minira <minira.samadova@syncthought.com>
2019-08-09 15:30:18 +0900

    63-trim-exclamations-accordingly

---
+++ 63-trim-exclamations-accordingly/63-trim-exclamations-accordingly.js
@@ -0,0 +1,31 @@
+// Description:
+// Remove some exclamation marks to keep the same number of exclamation marks
+// at the beginning and end of each word. Words are separated by spaces in the sentence.
+// Please note: only can remove, can not append.
+//
+// Examples
+// remove("Hi!") === "Hi"
+// remove("!Hi! Hi!") === "!Hi! Hi"
+// remove("!!Hi! !Hi!!") === "!Hi! !Hi!"
+// remove("!!!!Hi!! !!!!Hi !Hi!!!") === "!!Hi!! Hi !Hi!"
+// Note
+// Please don't post issue about difficulty or duplicate.
+
+
+function remove(string) {
+  let words = string.split(" ");
+  return words.map((word) => {
+    let start = word.match(/^[!]*/);
+    let end = word.match(/[!]*$/);
+    if (start[0].length > end[0].length) {
+      word = word.slice(start[0].length - end[0].length);
+    } else if (start[0].length < end[0].length) {
+      word = word.slice(0, start[0].length - end[0].length);
+    }
+    return word;
+  }).join(" ");
+};
+
+
+
+console.log(remove("!!!!Hi!! Hi!! !Hi!!! !!!Hi"));
